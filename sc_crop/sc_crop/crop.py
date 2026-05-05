fb4311801b3803968867f4124622478cc8fa9867
quentin revillon <quentin.revillon@gmail.com>
2026-05-05 15:42:06 -0400

    correct

--- sc_crop/sc_crop/crop.py
+++ sc_crop/sc_crop/crop.py
@@ -220,8 +220,8 @@ def aggregate_bbox_3d(preds: dict,
     rl1s, rl2s, ap1s, ap2s, zs = [], [], [], [], []
     for las_idx_inf, (cx, cy, w, h) in preds.items():
         z_nat   = min(Z_nat - 1, round(las_idx_inf / si_zoom))
-        rl_c    = (1.0 - cx) * RL_nat
-        ap_c    = (1.0 - cy) * AP_nat
+        rl_c    = cx * RL_nat
+        ap_c    = cy * AP_nat
         rl_half = w / 2 * RL_nat
         ap_half = h / 2 * AP_nat
         rl1s.append(max(0,      int(rl_c - rl_half)))
